7cde1dc2a3c124b7503456d46094cd857448f0c0
qshssewww <T1A1arxFpzP$>
2023-02-08 22:08:07 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/Containers/Header/Header";
 import Main from "./components/Containers/Main/Main";
 import Stars from "./components/Stars/Stars";
 import Footer from "./components/Containers/Footer/Footer";
+import Satellite from "./components/Satellite/Satellite";
 const App = () => {
-    const [isPageOpen, setIsPageOpen] = useState(false)
+    const [isPageOpen, setIsPageOpen] = useState(true)
     const arrowRef = useRef()
     const footerRef = useRef()
     return (
         <div className={'App'}>
+            <div className={'satellite'} id={'foxy'}></div>
+            <Satellite/>
             <Stars>
                 <Header footerRef={footerRef} arrowRef={arrowRef} setIsPageOpen={setIsPageOpen} isPageOpen={isPageOpen}/>
                 <Main arrowRef={arrowRef} isPageOpen={isPageOpen}/>
